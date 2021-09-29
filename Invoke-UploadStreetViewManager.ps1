a342b14f845f43eca8ef7d55a4fa72d823af7b99
Mike Boharsik <mboharsik@gmail.com>
2021-09-29 12:53:47 -0400

    chore: enhanced the verbose output of the upload script

--- Invoke-UploadStreetViewManager.ps1
+++ Invoke-UploadStreetViewManager.ps1
@@ -6,8 +6,16 @@ Param(
 	[switch] $OpenStagingPath
 )
 
+Write-Verbose "`$Dev = $Dev"
+Write-Verbose "`$KeepStagingFiles = $KeepStagingFiles"
+Write-Verbose "`$SkipUpload = $SkipUpload"
+Write-Verbose "`$OpenStagingPath = $OpenStagingPath"
+
 $configFilePath = "$PSScriptRoot\lambda.config.json"
+Write-Verbose "`$configFilePath = '$configFilePath'"
+
 $stagingPath = "$($env:temp)\street-view-manager\staging"
+Write-Verbose "`$stagingPath = '$stagingPath'"
 
 if ($OpenStagingPath) {
 	explorer (Join-Path $stagingPath "..")
@@ -18,7 +26,6 @@ $configs = ConvertFrom-Json -AsHashtable (Get-Content -Raw $configFilePath)
 
 if ($Dev) {
 	$configName = 'dev'
-	
 } else {
 	$configName = 'prod'
 }
@@ -52,21 +59,28 @@ try {
 	}
 
 	$uploadPackagePath = Join-Path $stagingPath "..\$lambdaFunctionName.zip"
+	Write-Verbose "`$uploadPackagePath = '$uploadPackagePath'"
 	if (Test-Path $uploadPackagePath) {
 		Remove-Item -Force $uploadPackagePath
 	}
 
-	Write-Verbose "Using '$appClientId' as OAuth client ID"
-
 	$env:REACT_APP_CLIENT_ID = $appClientId
+	Write-Verbose "`$env:REACT_APP_CLIENT_ID = '$env:REACT_APP_CLIENT_ID'"
+
+	$env:REACT_APP_KEY = $appKey
+	Write-Verbose "`$env:REACT_APP_KEY = '$env:REACT_APP_KEY'"
 
 	yarn build
+	Write-Verbose "Command 'yarn build' completed"
 	yarn git-hash
+	Write-Verbose "Command 'yarn git-hash' completed"
 
 	$buildPath = "$PSScriptRoot\build"
+	Write-Verbose "`$buildPath = '$buildPath'"
 	Copy-Item -Recurse $buildPath "$stagingPath\build"
 
 	$lambdaPath = "$PSScriptRoot\lambda"
+	Write-Verbose "`$lambdaPath = '$lambdaPath'"
 	Copy-Item -Recurse "$lambdaPath\**" $stagingPath
 
 	Remove-Item "$stagingPath\.gitignore" -ErrorAction SilentlyContinue
@@ -78,16 +92,21 @@ try {
 			| ConvertFrom-Json -AsHashtable
 			| Select-Object -ExpandProperty Functions
 			| Select-Object -ExpandProperty FunctionName
+			| Sort-Object
+
+		Write-Verbose "Found $($functionNames.length) functions associated with the current AWS account:`n$($functionNames -Join "`n")"
 
 		if (!$functionNames.Contains($lambdaFunctionName)) {
 			throw "Lambda function with name '$lambdaFunctionName' is not associated with the current AWS account"
 		}
 
-		aws lambda update-function-code --function-name $lambdaFunctionName --zip-file "fileb://$uploadPackagePath"
+		$uploadRes = aws lambda update-function-code --function-name $lambdaFunctionName --zip-file "fileb://$uploadPackagePath"
+		Write-Verbose "AWS lambda code update response:`n$($uploadRes | ConvertFrom-Json | ConvertTo-Json)"
 	}
 
 	if (!$KeepStagingFiles) {
 		Remove-Item -Force -Recurse (Join-Path $stagingPath "..")
+		Write-Verbose "Removed all files in path '$stagingPath'"
 	}
 } finally {
 	$env:REACT_APP_CLIENT_ID = $initAppClientId
