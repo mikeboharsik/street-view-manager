03cec9792201b5d26df9b2922d8fa1106ea1dbf8
Mike Boharsik <mboharsik@gmail.com>
2024-08-24 11:29:29 -0400

    test: refactor cypress tests into more of a natural language format

--- client/cypress/e2e/landing.cy.js
+++ client/cypress/e2e/landing.cy.js
@@ -1,3 +1,115 @@
+function whenUserNavigatesToRootPath() {
+	cy.visit('http://localhost:3000');
+}
+
+function thenAccessRequestTextExists() {
+	cy.contains('This application needs your permission to access the Street View content associated with your Google account');
+}
+
+function thenGrantAccessLinkExists() {
+	cy.get('[data-cy="link-grant-access"]').should('exist');
+}
+
+function thenLoaderExists() {
+	cy.get('#loader-container').should('exist');
+}
+
+function thenLoaderDoesNotExist() {
+	cy.get('#loader-container').should('not.exist');
+}
+
+function thenHeaderClassElementsExists() {
+	cy.get('.header').should('exist');
+}
+
+function thenFooterExists() {
+	cy.get('#footer-container').should('exist');
+}
+
+function thenLogoutButtonExists() {
+	cy.get('[data-cy="logout-button"]').should('exist');
+}
+
+function thenLogoutButtonDoesNotExist() {
+	cy.get('[data-cy="logout-button"]').should('not.exist');
+}
+
+function givenAccessTokenCookieExists() {
+	cy.setCookie('access_token', 'TEST_ACCESS_TOKEN');
+}
+
+function thenNoPhotosTextExists() {
+	cy.contains('No photos. Go ahead and upload some.');
+}
+
+function thenThumbnailsContainerExists() {
+	cy.get('#thumbnails-container').should('exist');
+}
+
+function thenThumbnailsContainerDoesNotExist() {
+	cy.get('#thumbnails-container').should('not.exist');
+}
+
+function thenPhotosNavigationContainerExists() {
+	cy.get('#photos-nav-container').should('exist');
+}
+
+function thenPhotosNavigationContainerDoesNotExist() {
+	cy.get('#photos-nav-container').should('not.exist');
+}
+
+function thenThumbnailOverlaysExist() {
+	cy.get('.thumbnail-overlay-container').should('exist');
+}
+
+function thenPageCountExists() {
+	cy.contains('1 / 1');
+}
+
+function givenGitVersionEndpointReturnsMockData() {
+	cy.intercept('GET', '/static/version.json', {
+		statusCode: 200,
+		body: 'some git hash',
+	}).as('gitVersion');
+}
+
+function givenPhotosEndpointReturnsNoPhotos() {
+	cy.intercept('GET', 'https://streetviewpublish.googleapis.com/v1/photos*', {
+		statusCode: 200,
+		body: {},
+	}).as('photos');
+}
+
+function givenPhotosEndpointReturnsFiveMockedPhotos() {
+	cy.intercept('GET', 'https://streetviewpublish.googleapis.com/v1/photos*', {
+		statusCode: 200,
+		body: {
+			photos: [
+				{ photoId: { id: 'testPhoto1' }, thumbnailUrl: '/mockThumbnailUrl' },
+				{ photoId: { id: 'testPhoto2' }, thumbnailUrl: '/mockThumbnailUrl' },
+				{ photoId: { id: 'testPhoto3' }, thumbnailUrl: '/mockThumbnailUrl' },
+				{ photoId: { id: 'testPhoto4' }, thumbnailUrl: '/mockThumbnailUrl' },
+				{ photoId: { id: 'testPhoto5' }, thumbnailUrl: '/mockThumbnailUrl' },
+			],
+		},
+	}).as('photos');
+}
+
+function givenThumbnailUrlReturnsMockedThumbnail() {
+	cy.intercept('GET', '/mockThumbnailUrl', {
+		statusCode: 200,
+		fixture: '../../src/images/thumbnail-placeholder.gif',
+	}).as('mockThumbnail');
+}
+
+function whenGitVersionResponseCompletes() {
+	cy.wait('@gitVersion');
+}
+
+function whenPhotosResponseCompletes() {
+	cy.wait('@photos');
+}
+
 describe('Landing page', () => {
 	describe('without auth cookie', () => {
 		afterEach(() => {
@@ -5,15 +117,13 @@ describe('Landing page', () => {
 		});
 
 		it('prompts the user to grant access', () => {
-			cy.visit('http://localhost:3000');
+			whenUserNavigatesToRootPath();
 
-			cy.contains('This application needs your permission to access the Street View content associated with your Google account');
-			cy.get('[data-cy="link-grant-access"]').should('exist');
-
-			cy.get('#loader-container').should('not.exist');
-
-			cy.get('#footer-container').should('exist');
-			cy.get('[data-cy="logout-button"]').should('not.exist');
+			thenAccessRequestTextExists();
+			thenGrantAccessLinkExists()
+			thenLoaderDoesNotExist();
+			thenFooterExists();
+			thenLogoutButtonDoesNotExist();
 		});
 	});
 
@@ -24,38 +134,27 @@ describe('Landing page', () => {
 			});
 
 			beforeEach(() => {
-				cy.setCookie('access_token', 'TEST_ACCESS_TOKEN');
-		
-				cy.intercept('GET', '/static/version.json', {
-					statusCode: 200,
-					body: 'some git hash',
-				}).as('gitVersion');
-		
-				cy.intercept('GET', 'https://streetviewpublish.googleapis.com/v1/photos*', {
-					statusCode: 200,
-					body: {},
-				}).as('photos');
+				givenAccessTokenCookieExists();
+
+				givenGitVersionEndpointReturnsMockData();
+				givenPhotosEndpointReturnsNoPhotos();
 			});
 		
 			it('loads with no photos', () => {
-				cy.visit('http://localhost:3000');
+				whenUserNavigatesToRootPath();
 		
-				cy.get('#loader-container').should('exist');
+				thenLoaderExists();
 		
-				cy.wait('@gitVersion');
-				cy.wait('@photos');
+				whenGitVersionResponseCompletes();
+				whenPhotosResponseCompletes();
 		
-				cy.get('#loader-container').should('not.exist');
-		
-				cy.contains('No photos. Go ahead and upload some.');
-	
-				cy.get('.header').should('exist');
-
-				cy.get('#footer-container').should('exist');
-				cy.get('[data-cy="logout-button"]').should('exist');
-		
-				cy.get('#thumbnails-container').should('not.exist');
-				cy.get('#photos-nav-container').should('not.exist');
+				thenLoaderDoesNotExist();
+				thenNoPhotosTextExists();
+				thenHeaderClassElementsExists();
+				thenFooterExists();
+				thenLogoutButtonExists();
+				thenThumbnailsContainerDoesNotExist();
+				thenPhotosNavigationContainerDoesNotExist();
 			});
 		});
 	
@@ -65,53 +164,29 @@ describe('Landing page', () => {
 			});
 
 			beforeEach(() => {
-				cy.setCookie('access_token', 'TEST_ACCESS_TOKEN');
+				givenAccessTokenCookieExists();
 		
-				cy.intercept('GET', '/static/version.json', {
-					statusCode: 200,
-					body: 'some git hash',
-				}).as('gitVersion');
-		
-				cy.intercept('GET', 'https://streetviewpublish.googleapis.com/v1/photos*', {
-					statusCode: 200,
-					body: {
-						photos: [
-							{ photoId: { id: 'testPhoto1' }, thumbnailUrl: '/mockThumbnailUrl' },
-							{ photoId: { id: 'testPhoto2' }, thumbnailUrl: '/mockThumbnailUrl' },
-							{ photoId: { id: 'testPhoto3' }, thumbnailUrl: '/mockThumbnailUrl' },
-							{ photoId: { id: 'testPhoto4' }, thumbnailUrl: '/mockThumbnailUrl' },
-							{ photoId: { id: 'testPhoto5' }, thumbnailUrl: '/mockThumbnailUrl' },
-						],
-					},
-				}).as('photos');
-	
-				cy.intercept('GET', '/mockThumbnailUrl', {
-					statusCode: 200,
-					fixture: '../../src/images/thumbnail-placeholder.gif',
-				}).as('mockThumbnail');
+				givenGitVersionEndpointReturnsMockData();
+				givenPhotosEndpointReturnsFiveMockedPhotos();
+				givenThumbnailUrlReturnsMockedThumbnail();
 			});
 		
 			it('loads with photos', () => {
-				cy.visit('http://localhost:3000');
+				whenUserNavigatesToRootPath();
 		
-				cy.get('#loader-container').should('exist');
+				thenLoaderExists();
 		
-				cy.wait('@gitVersion');
-				cy.wait('@photos');
+				whenGitVersionResponseCompletes();
+				whenPhotosResponseCompletes();
 		
-				cy.get('#loader-container').should('not.exist');
-	
-				cy.contains('1 / 1');
-		
-				cy.get('.header').should('exist');
-
-				cy.get('#footer-container').should('exist');
-				cy.get('[data-cy="logout-button"]').should('exist');
-		
-				cy.get('#thumbnails-container').should('exist');
-				cy.get('#photos-nav-container').should('exist');
-
-				cy.get('.thumbnail-overlay-container').should('exist');
+				thenLoaderDoesNotExist();
+				thenPageCountExists();
+				thenHeaderClassElementsExists();
+				thenFooterExists();
+				thenLogoutButtonExists();
+				thenThumbnailsContainerExists();
+				thenPhotosNavigationContainerExists();
+				thenThumbnailOverlaysExist();
 			});
 		});
 	});
